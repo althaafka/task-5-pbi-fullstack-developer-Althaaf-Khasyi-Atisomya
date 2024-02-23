2120432c9267a6f5ecc7204dd94941218543b1a7
althaafka <althaafka004@gmail.com>
2024-02-23 18:32:29 +0700

    feat: database setup

---
+++ database/setup.go
@@ -0,0 +1,19 @@
+package database
+import (
+	"gorm.io/gorm"
+	"gorm.io/driver/mysql"
+	"github.com/althaafka/task-5-pbi-fullstack-developer-Althaaf-Khasyi-Atisomya.git/models"
+)
+
+var DB *gorm.DB;
+
+func Connect() {
+	database, err := gorm.Open(mysql.Open("root:password@tcp(localhost:3306)/godb"))
+	if err != nil {
+		panic(err)
+	}
+
+	database.AutoMigrate(&models.User{})
+
+	DB = database
+}
